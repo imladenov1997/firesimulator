ae9dd39608a80f04da4ea11ab1327692520ad7ae
Ivo Mladenov <imladenov1997@gmail.com>
2017-05-04 19:50:51 +0100

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Ivo Mladenov
+ */
+public class Tree extends JPanel {
+    private int radius;
+    private int length;
+    private int width;
+
+    public Tree(int x, int y) {
+        super();
+        this.radius = 50;
+        this.length = 180;
+        this.width = 15;
+        this.setBounds(x, y, 300, 300);
+    }
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(new Color(153, 76, 0));
+        g2d.drawRect(150 - width, 300 - length, 2*width, length);
+        g2d.setColor(Color.GREEN);
+        g2d.drawOval(150 - radius, 300 - length - radius, 2*radius, 2*radius);
+    }
+}
