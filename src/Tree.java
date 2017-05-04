e9deb6167c104df6a2c1c9ddb3e5fbfe9f887e98
Teodor Nistor <teo.g.nistor@gmail.com>
2017-05-04 20:47:57 +0100

    tre

--- src/Tree.java
+++ src/Tree.java
@@ -19,7 +19,7 @@ public class Tree extends JPanel {
         this.width = 6;
         
         Random random = new Random();
-        this.setBounds(random.nextInt(w-300), random.nextInt(h-300), 300, 300);
+        this.setBounds(random.nextInt(w-100), random.nextInt(h-200), 100, 200);
         
 //        addMouseListener(new MouseAdapter() {
 //			@Override public void mouseClicked(MouseEvent e) {
@@ -35,8 +35,8 @@ public class Tree extends JPanel {
     public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(new Color(153, 76, 0));
-        g2d.fillRect(150 - width, 300 - length, 2*width, length);
+        g2d.fillRect(50 - width, 100 - length, 2*width, length);
         g2d.setColor(Color.GREEN);
-        g2d.fillOval(150 - radius, 300 - length - radius, 2*radius, 2*radius);
+        g2d.fillOval(50 - radius, 100 - length - radius, 2*radius, 2*radius);
     }
 }
